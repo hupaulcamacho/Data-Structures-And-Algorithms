baff40ad2ad9d9a5c7d9de1eb9d912a18231ca2d
Hupaul <hupaulcamacho@pursuit.org>
2020-09-02 19:38:49 -0400

    complete factorial

---
+++ HashTables/TwoSum.js
@@ -0,0 +1,9 @@
+// Given an array of integers nums and and integer target, return the indices of the two numbers such that they add up to target.
+
+const twoSum = function(nums, target) {
+    const numObj = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        numObj[i] = nums[i]
+    }
+};
---
+++ Recursion/Factorial.js
@@ -0,0 +1,13 @@
+// Write a factorial function that takes a positive integer, n as a parameter and prints the result of  n!(n factorial).
+
+const factorial = function(n) {
+    let result;
+    if (n <= 1) {
+        return 1
+    } else {
+        result = n * factorial(n - 1)
+        return result
+    }
+}
+
+console.log(factorial(4))
