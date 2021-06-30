88e056a74a14b13b490b73da48d81069b1e8bd00
traplordhuey <hupaulcamacho03@gmail.com>
2021-06-30 19:58:45 -0400

    Update MiddleNode.js

--- MiddleNode.js
+++ MiddleNode.js
@@ -1,6 +1,6 @@
 // Given a non-empty, singly linked list with head node head, return a middle node of linked list.
 
-const middleNode = function(head) => {
+const middleNode = function(head) {
   const pointerOne = head;
   const pointerTwo = head.next;
 
